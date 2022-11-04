45f6f891341fbab7b2da449bc0eb107dfd04db5c
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2022-11-04 10:12:25 +0100

    Creation campagne

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_URL: process.env.API_URL,
+    SERVICE_KEY: process.env.SERVICE_KEY,
+    SERVICE_ID: process.env.SERVICE_ID,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/create-campain',
+        destination: '/new-campain',
+        permanent: true,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
