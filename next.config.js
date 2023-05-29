a061141a4f9a7ec105d9627db6d6af43d347c7cf
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-05-29 12:26:55 +0200

    Asynchronous notifications

--- next.config.js
+++ next.config.js
@@ -14,7 +14,22 @@ const nextConfig = {
     GOOGLE_ID: process.env.GOOGLE_ID,
     GOOGLE_SECRET: process.env.GOOGLE_SECRET
   },
-
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`
+      },
+      {
+        source: '/items/:path*',
+        destination: `${process.env.BACKOFFICE_API}/items/:path*`
+      },
+      {
+        source: '/assets/:path*',
+        destination: `${process.env.BACKOFFICE_API}/assets/:path*`
+      },
+    ]
+  },
   async redirects() {
     return [
       {
