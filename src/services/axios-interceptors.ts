3c1c26b7335d9eda28ec26355675aefb75e0e5cf
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-06-03 12:43:45 +0200

    merge app

--- src/services/axios-interceptors.ts
+++ src/services/axios-interceptors.ts
@@ -22,7 +22,9 @@ const onRequest = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig
 
   const credentials = url.startsWith('/api/backoffice') ?  {} : { 'service-id': `${process.env.SERVICE_ID}`, 'service-key': `${process.env.SERVICE_KEY}`};
   const baseURL = url.startsWith('/api/backoffice') ? process.env.BACKOFFICE_API : `${process.env.API_URL}`;
-
+console.log('====================================');
+console.log({baseURL, url: urlToCall});
+console.log('====================================');
   return {
       ...config,
       baseURL,
