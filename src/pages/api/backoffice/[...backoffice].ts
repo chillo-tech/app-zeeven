37919d452d150b2a3c0b6419b2d253b2f0e5952e
Achille MBOUGUENG <achille.mbougueng@externe.maif.fr>
2023-07-25 06:26:49 +0200

    Affichage des champs des

--- src/pages/api/backoffice/[...backoffice].ts
+++ src/pages/api/backoffice/[...backoffice].ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from '@/services/axios-instance';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios, { AxiosError } from 'axios';
 
 async function handler(
   req: NextApiRequest,
@@ -7,11 +8,38 @@ async function handler(
 ) {
   try {
     const {url = ''} = req;
-    const {data} = await axiosInstance.get(url);
-    return res.status(200).json(data);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ message: "Requête invalide" });
+    const backendUrl = url.substring(url.indexOf('backend')) || '';
+    if (req.method === 'POST') {
+      const {data} = await axiosInstance.post(`${backendUrl}`, req.body);
+      return res.status(200).json(data);
+    } else if (req.method === 'PUT') {
+      const {data} = await axiosInstance.post(`${backendUrl}`, req.body);
+      return res.status(200).json(data);
+    } else if (req.method === 'PATCH') {
+      const {data} = await axiosInstance.patch(`${backendUrl}`, req.body);
+      return res.status(200).json(data);
+    }  else if (req.method === 'DELETE') {
+      const {data} = await axiosInstance.delete(`${backendUrl}`);
+      return res.status(200).json(data);
+    } else {
+      const {data} = await axiosInstance.get(`${backendUrl}`);
+      return res.status(200).json(data);
+    }
+  } catch (error: any) {
+    const axiosError = error as Error | AxiosError;
+    if(axios.isAxiosError(axiosError)){
+      const axiosError = error as Error | AxiosError;
+      if(axios.isAxiosError(axiosError)){
+        const {status, response} = error;
+      
+        if(status === 401 || (response && response.status && response.status === 401)) {
+          res.status(401).json({ message: "Veuillez vous connecter" });
+          return;
+        }
+      }
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
   }
 }
 
